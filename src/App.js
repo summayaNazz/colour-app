1b3df82a79d5e74f4505fd4b14f1f23e6aad7f72
summayaNazz <49797191+summayaNazz@users.noreply.github.com>
2019-07-17 10:09:27 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css;
+import Colourapp1 from './components/colourapp1';
 
-function App() {
+class App extends Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+ <Colourapp1 />
+   </div>
   );
+}
 }
 
 export default App;
